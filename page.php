50d0157cfdd3b927f73e49cfc83aec897287f40a
Helge Hofstad <helge.hofstad@gmail.com>
2016-02-07 22:41:23 +0100

    added Edit link to page template

--- page.php
+++ page.php
@@ -14,7 +14,18 @@
 
         <div class="row">
           <article id="post-<?php the_ID(); ?>" <?php post_class('col-lg-12 post_content'); ?> role="article">
+            <heder></heder>
+            <section></section>
 							<?php the_content( "Read more &raquo;" ); ?>
+              <?php
+                edit_post_link(
+                  sprintf('Edit<span class="screen-reader-text"> "%s"</span>',
+                    get_the_title()
+                  ),
+                  '<span class="edit-link">',
+                  '</span>'
+                );
+              ?>
 					</article>
         </div>
 

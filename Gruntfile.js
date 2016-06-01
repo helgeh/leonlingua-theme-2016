68652e7e0b25ea710926d7c160208c84ffafbfe6
Helge Hofstad <helge.hofstad@gmail.com>
2016-06-01 23:25:01 +0200

    better variable name

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    remote: paths.test,
+    serverpath: paths.test,
     bump: {
 	    options: {
 	      files: ['package.json', 'style.css'],
@@ -26,7 +26,7 @@ module.exports = function(grunt) {
 	      options: {
 					authKey: 'authKey1',
 	    		host: 'ftp.domeneshop.no',
-	    		dest: '<%= remote %>',
+	    		dest: '<%= serverpath %>',
 	    		port: 21
 	      },
 	      files: [
@@ -48,7 +48,7 @@ module.exports = function(grunt) {
 
   grunt.registerTask('upload', 'Upload files to ftp server.', function(target) {
     if (target === 'prod') {
-    	grunt.config.set('remote', paths.prod);
+    	grunt.config.set('serverpath', paths.prod);
     }
     grunt.task.run(['ftp_push:deploy']);
   });

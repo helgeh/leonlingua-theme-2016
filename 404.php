1ca74a5d904b14a7fd49d55545dad58c1d04764b
Helge Hofstad <helge.hofstad@gmail.com>
2017-04-13 22:45:28 +0200

    add proper 404 page

---
+++ 404.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+			<div class="main-section">
+
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="page-header">
+              <h1>Page not found!</h1>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-12">
+	        	<p>
+	        		Sorry, we could not find what you were looking for...
+	        	</p>
+	        </div>
+        </div>
+
+      </div>
+
+<?php get_footer(); ?>

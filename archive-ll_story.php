08c213e8f3d54d00fc2dc5edc2031b457eb0cd2f
Helge Hofstad <helge.hofstad@gmail.com>
2017-04-14 20:39:55 +0200

    new: add custom fields to Testimonials CPT

--- archive-ll_story.php
+++ archive-ll_story.php
@@ -10,21 +10,27 @@
           </div>
         </div>
 
-        <div class="row">
-          <div class="col-md-12">
-            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-              <article id="post-<?php the_ID(); ?>" <?php post_class('post_content'); ?> role="article">
-                <header>
-                  <?php the_title( sprintf( '<h2 class="page-title screen-reader-text"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-                </header>
-                <section>
-                  <?php the_excerpt(); ?>
-                </section>
-                <footer></footer>
-    					</article>
-            <?php endwhile; endif; ?>
+        <?php $reverse = TRUE; ?>
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+          <div class="row">
+            <div class="col-md-12">
+            <article id="post-<?php the_ID(); ?>" <?php post_class('post_content'); ?> role="article">
+              <?php $reverse = !$reverse; if ($reverse) : ?>
+                <blockquote>
+              <?php else : ?>
+                <blockquote class="blockquote-reverse">
+              <?php endif; ?>
+                <?php the_field('content'); ?>
+                <?php if (get_field( 'author' )) : ?>
+                  <footer>
+                     <?php the_field('author'); ?>
+                  </footer>
+                <?php endif; ?>
+              </blockquote>
+  					 </article>
+            </div>
           </div>
-        </div>
+        <?php endwhile; endif; ?>
 
       </div>
 
--- testimonials-post-types.php
+++ testimonials-post-types.php
@@ -1,6 +1,10 @@
 <?php
 
 
+define( 'ACF_LITE', true );
+include_once('advanced-custom-fields/acf.php');
+
+
 add_action( 'init', 'create_posttype' );
 function create_posttype()
 {
@@ -18,4 +22,97 @@ function create_posttype()
       'menu_position' => 20
     )
   );
+}
+
+if (function_exists("register_field_group"))
+{
+	register_field_group(array (
+		'id' => 'acf_testimonial',
+		'title' => 'Testimonial',
+		'fields' => array (
+			array (
+				'key' => 'field_58f0f64549a8c',
+				'label' => 'Blurb',
+				'name' => 'blurb',
+				'type' => 'text',
+				'instructions' => 'Short, expressive excerpt of a customers testimonial. Max two sentences.',
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'none',
+				'maxlength' => 120,
+			),
+			array (
+				'key' => 'field_58f0f70549a8d',
+				'label' => 'Content',
+				'name' => 'content',
+				'type' => 'textarea',
+				'default_value' => '',
+				'placeholder' => '',
+				'maxlength' => '',
+				'rows' => '',
+				'formatting' => 'br',
+			),
+			array (
+				'key' => 'field_58f0f73c49a8e',
+				'label' => 'Author',
+				'name' => 'author',
+				'type' => 'text',
+				'instructions' => 'Name of customer providing the testimonial',
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'none',
+				'maxlength' => '',
+			),
+			array (
+				'key' => 'field_58f0f78549a8f',
+				'label' => 'Featured',
+				'name' => 'featured',
+				'type' => 'number',
+				'instructions' => 'Set a number higher than zero to make the excerpt of this testimonial go on the front page. Their order is determined by these numbers.',
+				'default_value' => 0,
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'min' => '',
+				'max' => '',
+				'step' => '',
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'll_story',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'normal',
+			'layout' => 'no_box',
+			'hide_on_screen' => array (
+				0 => 'permalink',
+				1 => 'the_content',
+				2 => 'excerpt',
+				3 => 'custom_fields',
+				4 => 'discussion',
+				5 => 'comments',
+				6 => 'revisions',
+				7 => 'slug',
+				8 => 'author',
+				9 => 'format',
+				10 => 'featured_image',
+				11 => 'categories',
+				12 => 'tags',
+				13 => 'send-trackbacks',
+			),
+		),
+		'menu_order' => 0,
+	));
 }
